332ef9d4d1db08d17a2bee1241a26bc7f8f79ad2
Jelani Harris <407760+jelaniharris@users.noreply.github.com>
2024-04-05 17:54:00 -0500

    Added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,41 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Tower of Greed',
+  generator: 'Next.js',
+  applicationName: 'Next.js',
+  keywords: [
+    'dungeon',
+    'crawler',
+    'game',
+    'browser',
+    'next.js',
+    'react',
+    'typescript',
+    'javascript',
+  ],
+  creator: 'Jelani Harris',
   description:
-    'A dungeon crawler you can play in browser. Created by Jelani Harris.',
+    'A dungeon crawler you can play in your browser. Created by Jelani Harris.',
+  category: 'Games',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    siteName: 'Tower of Greed',
+    url: 'https://tower-of-greed.vercel.app/',
+    title: 'Tower of Greed',
+    alternateLocale: ['en_US'],
+    description:
+      'A dungeon crawler you can play in your browser. Created by Jelani Harris.',
+    images: [
+      {
+        url: 'https://tower-of-greed.vercel.app/textures/Tower of Greed Logo.png',
+        width: 1200,
+        height: 630,
+        alt: 'Tower of Greed',
+      },
+    ],
+  },
+  robots: 'index, follow',
 };
 
 export default function RootLayout({
